871c100f61521fc15eb697b0cbb68d15b4a6bcba
MittaniK1 <m.keichi16256@gmail.com>
2021-06-25 14:45:50 +0900

    [add] xml file [remove] txt file XMLによるツイートの読み書きを設定

--- index.php
+++ index.php
@@ -1,38 +1,48 @@
 <?php
+date_default_timezone_set('Asia/Tokyo');
 $title = "MyTweet";
-$fname = "tweet.txt";
-if(isset($_POST['tweet'])){
-    file_put_contents($fname, $_POST['tweet']."\n", FILE_APPEND);
+$xmlfile = "tweets.xml";
+if (isset($_POST['tweet'])) {
+    $now = date("Y-m-d H:i:s");
+    $tweets = simplexml_load_file($xmlfile);
+    $newid = $tweets->count() + 1;
+    $entry = $tweets->addChild("entry");
+    $entry->addAttribute("id", $newid);
+    $entry->addChild("date", $now);
+    $entry->addChild("text", $_POST['tweet']);
+    file_put_contents($xmlfile, $tweets->asXML());
 }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; character=UTF-8">
-        <title><?php echo $title; ?></title>
-    </head>
-    <body>
-        <h1><?php echo $title; ?></h1>
-        <form action="index.php" method="post">
-            <div>
-                <label>
-                    <textarea name="tweet" placeholder="いまどうしてる？"></textarea>
-                </label>
-            </div>
-            <input type="submit"/>
-        </form>
-        <div>
-            <?php
-            if (file_exists($fname)){
-                $tweets = file_get_contents($fname);
-                $tweets  =explode("\n", $tweets);
-                for($i = 0; $i < count($tweets); $i++){
-                    echo "<div>".$tweets[$i]."</div>";
-                }
-            }
-            ?>
-        </div>
-    </body>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; character=UTF-8">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+<h1><?php echo $title; ?></h1>
+<form action="index.php" method="post">
+    <div>
+        <label>
+            <textarea name="tweet" placeholder="いまどうしてる？"></textarea>
+        </label>
+    </div>
+    <input type="submit"/>
+</form>
+<div>
+    <?php
+    if (file_exists($xmlfile)) {
+        $tweets = simplexml_load_file($xmlfile);
+        foreach ($tweets as $entry) {
+            echo "<div class='entry'>";
+            echo "<div class='date'><p>$entry->date</p></div>";
+            echo "<div class='text'><h3>$entry->text</h3></div>";
+            echo "</div>\n";
+        }
+    }
+    ?>
+</div>
+</body>
 </html>
 
 
