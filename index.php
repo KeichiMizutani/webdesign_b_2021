ccfea5b30626429b0a4324e67845f92c9f5c1968
MittaniK1 <m.keichi16256@gmail.com>
2021-07-02 13:23:45 +0900

    [fix] Dockerで操作する用保存

--- index.php
+++ index.php
@@ -2,6 +2,32 @@
 date_default_timezone_set('Asia/Tokyo');
 $title = "MyTweet";
 $xmlfile = "tweets.xml";
+/*
+@session_start();
+if (isset($_SESSION['dirname']) && $_SESSION['dirname'] == dirname($_SERVER['SCRIPT_NAME'])) {
+    $login = true;
+} else {
+    $login = false;
+}
+
+if ($login && isset($_POST['tweet'])) {
+    $now = date("Y-m-d H:i:s");
+    $tweets = simplexml_load_file($xmlfile);
+    $newid = $tweets->count() + 1;
+    $entry = $tweets->addChild("entry");
+    $entry->addAttribute("id", $newid);
+    $entry->addChild("date", $now);
+    $entry->addChild("text", $_POST['tweet']);
+    if (is_uploaded_file($_FILES['image']['tmp_name'])) {
+        $tmpfile = $_FILES['image']['tmp_name'];
+        $imgfile = "./img/" . $newid . "_" . $_FILES['image']['name'];
+        move_uploaded_file($tmpfile, $imgfile);
+        $entry->addChild("img", $imgfile);
+    }
+    file_put_contents($xmlfile, $tweets->asXML());
+}
+*/
+
 if (isset($_POST['tweet'])) {
     $now = date("Y-m-d H:i:s");
     $tweets = simplexml_load_file($xmlfile);
@@ -18,6 +44,7 @@ if (isset($_POST['tweet'])) {
     }
     file_put_contents($xmlfile, $tweets->asXML());
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -29,6 +56,11 @@ if (isset($_POST['tweet'])) {
 
 <body>
 <h1><?php echo $title; ?></h1>
+
+<?php
+/*
+if ($login) {
+    echo <<<HTML
 <form action="index.php" method="POST" enctype="multipart/form-data">
     <div>
         <textarea name="tweet" placeholder="いまどうしてる？"></textarea>
@@ -38,28 +70,43 @@ if (isset($_POST['tweet'])) {
     </div>
     <input type="submit"/>
 </form>
-    <div>
-        <?php
-        if (file_exists($xmlfile)) {
-            $tweets = simplexml_load_file($xmlfile);
-            foreach ($tweets as $entry) {
-                $entries[(string)$entry['id']] = $entry;
-            }
+HTML;
+} else {
+    echo "<a href='login.php'>ログイン</a>";
+}
+*/
 
-            krsort($entries);
-            foreach ($entries as $entry) {
-                echo "<div class='entry'>";
-                echo "<div class='date'><p>$entry->date</p></div>";
-                echo "<div class='text'><h3>$entry->text</h3></div>";
-                if ($entry->img != "") {
-                    echo "<img src='" . $entry->img . "'/>";
-                }
-                echo "</div>" . "\n";
-            }
-        }
-        ?>
+echo <<<HTML
+    <form action="index.php" method="POST" enctype="multipart/form-data">
+    <div>
+        <textarea name="tweet" placeholder="いまどうしてる？"></textarea>
     </div>
+    <div>
+        <input type="file" name="image" accept="image/gif,image/jpeg,image/jpg,image/png,image/PNG"/>
+    </div>
+    <input type="submit"/>
+HTML;
+?>
+<div>
+    <?php
+    if (file_exists($xmlfile)) {
+        $tweets = simplexml_load_file($xmlfile);
+        foreach ($tweets as $entry) {
+            $entries[(string)$entry['id']] = $entry;
+        }
+
+        krsort($entries);
+        foreach ($entries as $entry) {
+            echo "<div class='entry'>";
+            echo "<div class='date'><p>$entry->date</p></div>";
+            echo "<div class='text'><h3>$entry->text</h3></div>";
+            if ($entry->img != "") {
+                echo "<img src='" . $entry->img . "'/>";
+            }
+            echo "</div>" . "\n";
+        }
+    }
+    ?>
+</div>
 </body>
 </html>
-
-
